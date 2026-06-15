613918029a0b59770c9f97b3b409468156849475
plusdojo2026-nakano <nakano-nozomi-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 09:10:46 +0900

    Update servlet

--- source/src/main/java/servlet/HomeAdminServlet.java
+++ source/src/main/java/servlet/HomeAdminServlet.java
@@ -2,13 +2,11 @@ package servlet;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class HomeAdminServlet
@@ -20,31 +18,10 @@ public class HomeAdminServlet extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-		//もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/c3/LoginServlet");
-			return;
-		}
-		// 登録ページにフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/admin_regist.jsp");
-		dispatcher.forward(request, response);
-	}
-    public HomeAdminServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
