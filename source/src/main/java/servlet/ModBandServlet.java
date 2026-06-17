2205b0b239fce9e0cbb0cb3b95ee671a840245bc
plusdojo2026-morino <morino-yuka-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-17 14:11:52 +0900

    盛野友香

---
+++ source/src/main/java/servlet/ModBandServlet.java
@@ -0,0 +1,139 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.BandInfoDao;
+import dao.BandMemberDao;
+import dao.PartsDao;
+import dto.BandInfo;
+import dto.BandMember;
+import dto.Parts;
+import dto.Result;
+
+@WebServlet("/ModBandServlet")
+public class ModBandServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+//		// ログインしていなかったらログインサーブレットへ
+//		HttpSession session = request.getSession();
+//		if (session.getAttribute("id") == null) {
+//			response.sendRedirect("/c3/LoginServlet");
+//			return;
+//		}
+		
+		// 登録されているデータがあるか検索し、登録されたデータがあった場合その内容をリクエストスコープに登録する
+//		BandInfoDao biDao = new BandInfoDao();
+		BandMemberDao bmDao = new BandMemberDao();
+		PartsDao ptDao = new PartsDao();
+		BandInfo bi = new BandInfo();
+		List<BandMember> bmList = new ArrayList<BandMember>();
+		List<Parts> partsList = new ArrayList<Parts>();
+		
+//		bi = biDao.showBandInfo((LoginUser)session.getAttribute("id"));
+		partsList = ptDao.showAllParts();
+		
+		int biNum = 1;
+		bmList = bmDao.showMember(biNum);
+		request.setAttribute("band_info_id", biNum);
+		request.setAttribute("band_info_name", "バンドA");
+		request.setAttribute("band_members", bmList);
+		request.setAttribute("parts", partsList);
+		
+//		if (bi != null) {
+////			bmList = bmDao.showMember(bi);
+//			request.setAttribute("band_info", bi);
+//			request.setAttribute("band_members", bmList);
+//			request.setAttribute("parts", partsList);
+//		}
+		
+		// バンド情報画面へフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mod_band.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+//		// ログインしていなかったらログインサーブレットへ
+//		HttpSession session = request.getSession();
+//		if (session.getAttribute("id") == null) {
+//			response.sendRedirect("/c3/LoginServlet");
+//			return;
+//		}
+		
+//		LoginUser user = (LoginUser)session.getAttribute("id");
+		BandMemberDao bmDao = new BandMemberDao();
+		BandInfoDao biDao = new BandInfoDao();
+		boolean result = false;
+		
+		// 既にあるバンド情報か確認し、無ければ登録を行う
+		// band_idが0なら無い、1以上なら登録されている。
+		int bandId = Integer.parseInt(request.getParameter("band_id"));
+		// メンバーの最大値を取る
+		int memberNum = Integer.parseInt(request.getParameter("member_num"));
+		
+		
+		if (bandId == 0) {	// 未登録のバンドである場合
+			// 登録する
+			// バンド情報テーブル
+			String name = request.getParameter("band_name");
+//			result = biDao.addBand(new BandInfo(0, name, user.getId));
+					
+			// メンバーの登録
+			for (int i = 0; i <= memberNum; i++) {
+				String memberName = request.getParameter("member_name[" + i + "]");
+				int partId = Integer.parseInt(request.getParameter("parts[" + i + "]"));
+				
+				result = bmDao.addMember(new BandMember(0, memberName, partId, bandId));
+				if (!result)
+					break;
+			}
+			
+		} else if (bandId > 0) {	// 既に登録されているバンドである場合
+			// 編集する
+			// バンド情報テーブル
+			String name = request.getParameter("band_name");
+//			result = biDao.editBand(new BandInfo(bandId, name, user.getId));
+			
+			// バンドメンバーを一旦削除してから再度登録する
+			// 削除する
+			List<BandMember> bmList = new ArrayList<BandMember>();
+			bmList = bmDao.showMember(bandId);
+			for (BandMember bm : bmList) {
+				result = bmDao.deleteMember(bm);
+			}
+			
+			// 登録する
+			for (int i = 0; i <= memberNum; i++) {
+				String memberName = request.getParameter("member_name[" + i + "]");
+				int partId = Integer.parseInt(request.getParameter("parts[" + i + "]"));
+				
+				result = bmDao.addMember(new BandMember(0, memberName, partId, bandId));
+				if (!result)
+					break;
+			}
+			
+		}
+		
+		// ホームサーブレットへ戻る。
+		if (result) {
+			response.sendRedirect("/c3/HomeBandServlet");
+		} else {
+			request.setAttribute("result", new Result("Mod_failed", "登録できませんでした。", "/c3/ModBandServlet"));
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mod_band.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+}
---
+++ source/src/main/webapp/javascript/mod_band.js
@@ -0,0 +1,77 @@
+'use strict';
+
+let band_member = document.getElementById('band_member');
+let rowIndex = document.querySelectorAll("#bands_info_row.band_info_row").length;
+
+if (rowIndex === 0) {
+	rowIndex = 1;
+}
+
+// 入力欄を増やす
+function addRow() {
+    // 対象テーブルを取得
+    const table = document.getElementById("members_info_row");
+    const template = document.getElementById("band_row_template");
+    
+    // テンプレートをコピー
+    const clone = template.content.cloneNode(true);
+    
+    // コピーした要素のname属性を現在のrowIndexで上書き
+    clone.querySelector('input[name="member_id_temp"]').name = `member_id[${rowIndex}]`;
+    clone.querySelector('input[name="membername_temp"]').name = `member_name[${rowIndex}]`;
+    clone.querySelector('select[name="parts_temp"]').name = `parts[${rowIndex}]`;
+    clone.querySelector('span[class="memo_temp"]').value = `${rowIndex}`;
+    
+    table.appendChild(clone);
+    
+    const memberNum = table.querySelector('input[name="member_num"]');
+    if (memberNum) {
+        memberNum.value = rowIndex;
+    }
+    rowIndex++;
+}
+
+// 入力欄を消す
+function removeRow(btn) {
+    let row = btn.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+
+// 空欄エラー
+band_member.onsubmit = function(event) {
+    let blank = false;
+    let setMemberName;
+    let setMemberParts;
+    
+    for (let i = 0; i < rowIndex; i++) {
+        setMemberName = `member_name[${i}]`;
+        setMemberParts = `parts[${i}]`;
+        console.log(setMemberName);
+        console.log(setMemberParts);
+
+        const memberName = band_member.querySelector('input[name="' + setMemberName + '"]');
+        const memberParts = band_member.querySelector('select[name="' + setMemberParts + '"]');
+        console.log(memberName);
+        console.log(memberParts);
+
+        if (memberName && memberParts){
+            if (memberName.value === '' || memberParts.value === '') {
+                blank = true;
+                break;
+            } else {
+                blank = false;
+            }
+        }
+    }
+
+    if (band_member.band_name.value === '' || blank) {
+        document.getElementById('blank_alert').textContent = '未入力の項目があります。';
+        event.preventDefault();
+    } else if (document.querySelectorAll("#bands_info_row.band_info_row").length === 0){
+		document.getElementById('blank_alert').textContent = 'バンドメンバーを最低1人登録してください。';
+		event.preventDefault();
+    } else {
+        document.getElementById('blank_alert').textContent = '';
+        window.alert('一件のバンド情報を登録しました');
+    }
+};
