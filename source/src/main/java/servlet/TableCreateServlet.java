965eff65eb00dc59eccf95cb679db71e41eb197e
plusdojo2026-morino <morino-yuka-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 17:57:26 +0900

    Update TableCreateServlet.java

--- source/src/main/java/servlet/TableCreateServlet.java
+++ source/src/main/java/servlet/TableCreateServlet.java
@@ -52,26 +52,13 @@ public class TableCreateServlet extends HttpServlet {
 	
 	// 準備情報をもとにバンド情報を持ってくる
 	for (PreparInfo pi : piList) {
-		System.out.println("pi:" + pi.getId() + " bi:"+ pi.getBandInfoId());
 		biList.addAll(biDao.select(new BandInfo(pi.getBandInfoId(), "", 0)));
-		System.out.println("biList要素数:" + biList.size());
 	}
 
 	// それぞれをデータとして渡す
 	request.setAttribute("live_info", li);
 	request.setAttribute("band_infos", biList);
 	request.setAttribute("prepar_infos", piList);
-
-	// 確認
-	System.out.println("ライブ名：" + li.getName());
-	System.out.println("バンド名表示");
-	for(BandInfo b:biList) {
-		System.out.println("ライブ[" + li.getId() + "]" +b.getId() + ":" + b.getName());
-	}
-	System.out.println("準備情報表示");
-	for(PreparInfo p:piList) {
-		System.out.println("[" + p.getSetlist() + "]" + p.getId() + ":" + p.getBandInfoId());
-	}
 	
 	// each_musicの情報を取得して渡す
 	for (int i = 0; i < piList.size(); i++) {
@@ -80,10 +67,6 @@ public class TableCreateServlet extends HttpServlet {
 		emList = emDao.select(piList.get(i));
 		request.setAttribute(name, emList);
 		
-		// 確認
-		for (EachMusic e: emList) {
-			System.out.println(piList.get(i).getId() + ":[" + e.getSetlist() + "]" + e.getName());
-		}
 	}
 	
 	// タイムテーブル作成画面へフォワードする
