d888f5b36ee37fd32acffe91f3f92b185ee08d87
plusdojo2026-STanio <tanio-shun-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-25 13:41:00 +0900

    web

--- source/src/main/java/dao/UserDao.java
+++ source/src/main/java/dao/UserDao.java
@@ -19,9 +19,9 @@ public class UserDao {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/C3?"
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/c3?"
 					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
+					"c3", "zTfP4Ep4RMwQge3E");
 			
 			// SELECT文を準備する
 			String sql = "SELECT user_id, password, type FROM users WHERE user_id=? AND password=?";
